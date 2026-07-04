2f74de3da95db95ff4e92bed73b6ad902558c90c
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2026-07-04 19:21:50 -0400

    removed all of the return statements

--- script.js
+++ script.js
@@ -11,31 +11,30 @@ function check() {
    // Get input from variable
    const password = document.getElementById("passwordInput").value;
 
-   if (password.value == "") {
+   if (password.value === "") {
       alert("Please enter a password.");
       document.getElementById("passwordScore").style.color = `red`;
       score = 0;
-      return;
+
    } else if (password.length < 8) {
       alert("Password must be at least 8 characters.");
       document.getElementById("passwordScore").style.color = `red`;
       score = 25;
-      return;
+
    } else if (password.match(int) && password.match(upperCaseLetters)) {
       alert("Password only has int and upperCaseLetters");
       document.getElementById("passwordScore").style.color = `orange`;
       score = 50;
-      return;
+
    } else if(password.match(int) && password.match(upperCaseLetters) && password.match(lowerCaseLetters)) {
       alert("Password has int, upperCaseLetters, and lowerCaseLetters");
       document.getElementById("passwordScore").style.color = `yellow`;
       score = 75;
-      return;
+
    } else if (password.match(int) && password.match(upperCaseLetters) && password.match(lowerCaseLetters) && password.match(symbols)) {
       alert("Password is secured");
       document.getElementById("passwordScore").style.color = `green`;
       score = 100;
-      return;
    }
 
    document.getElementById("passwordScore").innerHTML = `Password Strength: ${score}%`;

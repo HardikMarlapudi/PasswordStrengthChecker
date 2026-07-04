033fac19e71d6f1275f6d3b781bb23ea3e4252ab
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2026-07-04 13:47:19 -0400

    Wrote the JavaScript code, but neeed to debug it even further

---
+++ script.js
@@ -0,0 +1,42 @@
+// script.js
+function check() {
+
+   // Password variables including score counter
+   let score = 0;
+   let int = /[0-9]/;
+   let upperCaseLetters = /[A-Z]/;
+   let lowerCaseLetters = /[a-z]/;
+   let symbols = /[!@#$%^&*()]/;
+
+   // Get input from variable
+   const password = document.getElementById("passwordInput").value;
+
+   if (password.value == "") {
+      alert("Please enter a password.");
+      document.getElementById("passwordScore").style.color = `red`;
+      score = 0;
+      return;
+   } else if (password.length < 8) {
+      alert("Password must be at least 8 characters.");
+      document.getElementById("passwordScore").style.color = `red`;
+      score = 25;
+      return;
+   } else if (password.match(int) && password.match(upperCaseLetters)) {
+      alert("Password only has int and upperCaseLetters");
+      document.getElementById("passwordScore").style.color = `orange`;
+      score = 50;
+      return;
+   } else if(password.match(int) && password.match(upperCaseLetters) && password.match(lowerCaseLetters)) {
+      alert("Password has int, upperCaseLetters, and lowerCaseLetters");
+      document.getElementById("passwordScore").style.color = `yellow`;
+      score = 75;
+      return;
+   } else if (password.match(int) && password.match(upperCaseLetters) && password.match(lowerCaseLetters) && password.match(symbols)) {
+      alert("Password is secured");
+      document.getElementById("passwordScore").style.color = `green`;
+      score = 100;
+      return;
+   }
+
+   document.getElementById("passwordScore").innerHTML = `Password Strength: ${score}%`;
+};
